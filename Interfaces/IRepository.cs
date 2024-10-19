e56484dc199ed00acc8448f41e8097dc52f28ac4
paulorudiger <paulo.e.rudiger@gmail.com>
2024-10-19 19:58:55 -0300

    Implementada interface IRepository

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -1,19 +1,24 @@
 ﻿using POO_A4.Database;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace POO_A4.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-
         //  protected readonly PetDbContext _context;
 
-     //   protected abstract void ValidateAdd();
+        //   protected abstract void ValidateAdd();
         //protected abstract void ValidateDelete();
 
+        public void Add(T entity);
 
+        public void Update(T entity);
 
+        public void Delete(T entity);
 
+        public T GetById(int id);
 
-
+        public IEnumerable<T> GetAll();
     }
 }
